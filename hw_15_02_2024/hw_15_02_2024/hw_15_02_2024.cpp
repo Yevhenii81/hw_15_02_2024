9817546dad8ad4cd32e2ab6663a38ca7ba99f61a
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2024-02-26 19:08:01 +0200

    Add files via upload

---
+++ hw_15_02_2024/hw_15_02_2024/hw_15_02_2024.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+void Verification(string math)
+{
+	stack<char> parentheses;
+
+	parentheses.push(math[0]);
+
+	for (int i = 1; i < math.length(); i++)
+	{
+		if (math[i] == '(' || math[i] == '[' || math[i] == '{')
+		{
+			parentheses.push(math[i]);
+		}
+		else if (math[i] == ')')
+		{
+			if (parentheses.top() == '(')
+				parentheses.pop();
+			else
+				break;
+		}
+		else if (math[i] == ']')
+		{
+			if (parentheses.top() == '[')
+				parentheses.pop();
+			else
+				break;
+		}
+		else if (math[i] == '}')
+		{
+			if (parentheses.top() == '{')
+				parentheses.pop();
+			else
+				break;
+		}
+	}
+
+	if (parentheses.empty())
+	{
+		cout << "correct expression" << "\n";
+
+	}
+	else
+	{
+		cout << "wrong expression" << "\n";
+	}
+};
+
+int main()
+{
+	string mathFirst = "{(5-[3+<9>])-4}";
+	cout << mathFirst << " - ";
+	Verification(mathFirst);
+
+	string mathSecond = "{(<5->[3 +)9-4]}";
+	cout << mathSecond << " - ";
+	Verification(mathSecond);
+}
